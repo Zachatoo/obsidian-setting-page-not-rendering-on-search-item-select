fa24b378bef7bbe1d411508f72d825c3380ae551
Zachatoo <Zachatoo@users.noreply.github.com>
2026-05-30 15:04:49 -0600

    Init and prep for report

--- src/settings.ts
+++ src/settings.ts
@@ -1,38 +1,88 @@
-import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import {
+	App,
+	PluginSettingTab,
+	SettingDefinitionItem,
+	SettingGroup,
+	SettingPage,
+} from 'obsidian';
+import SettingPageNotRenderingOnSearchItemSelectPlugin from './main';
 
-export interface MyPluginSettings {
+export interface SettingPageNotRenderingOnSearchItemSelectPluginSettings {
 	mySetting: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default',
-};
+export const DEFAULT_SETTINGS: SettingPageNotRenderingOnSearchItemSelectPluginSettings =
+	{
+		mySetting: 'default',
+	};
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class SettingTab extends PluginSettingTab {
+	plugin: SettingPageNotRenderingOnSearchItemSelectPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(
+		app: App,
+		plugin: SettingPageNotRenderingOnSearchItemSelectPlugin,
+	) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
+	getSettingDefinitions(): SettingDefinitionItem[] {
+		return [
+			{
+				type: 'page',
+				name: 'Items',
+				desc: 'Rendered with "items"',
+				items: [
+					{
+						type: 'group',
+						name: 'Item',
+						desc: 'Rendered with "items"',
+						items: [
+							{
+								name: 'Item',
+								desc: 'Rendered with "items"',
+								control: {
+									type: 'text',
+									key: 'mySetting',
+								},
+							},
+						],
+					},
+				],
+			},
+			{
+				type: 'page',
+				name: 'Page',
+				desc: 'Rendred with "page"',
+				page: () => new MySettingPage(this),
+			},
+		];
+	}
+}
+
+class MySettingPage extends SettingPage {
+	constructor(private pluginSettingTab: SettingTab) {
+		super();
+	}
+
 	display(): void {
-		const { containerEl } = this;
+		this.containerEl.empty();
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter your secret')
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+		new SettingGroup(this.containerEl).addSetting((setting) => {
+			setting
+				.setName('Item')
+				.setDesc('Rendered with "page"')
+				.addText((cb) => {
+					cb.setValue(
+						this.pluginSettingTab.plugin.settings.mySetting,
+					).onChange(async (value) => {
+						await this.pluginSettingTab.setControlValue(
+							'mySetting',
+							value,
+						);
+					});
+				});
+		});
 	}
 }
